491bae73aae0ae86309770cf3f3b18377d395700
VanyaS26 <vanyasynytsia@gmail.com>
2026-03-29 09:59:27 +0300

    укенгнек

---
+++ Assets/Serealizator/Serealizator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.IO;
+
+public class Serealizator : MonoBehaviour
+{
+    public static Serealizator instance;
+
+    private void Awake() {  instance = this; }
+
+    public void SaveToTxt()
+    {
+        
+    }
+}
