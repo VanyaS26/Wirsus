a43f80ab00741ed22135ebbb5c8bf83237f3ab8b
VanyaS26 <vanyasynytsia@gmail.com>
2026-03-30 20:47:26 +0300

    defghjk

---
+++ Assets/FileTypes/DiskWithoutMono.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+public class DiskWithoutMono
+{
+    public char nameDisk;
+    public int foldersCount;
+
+    public List<FolderWithoutMono> list = new List<FolderWithoutMono>();
+}
--- Assets/FileTypes/TypeDataDisk.cs
+++ Assets/FileTypes/TypeDataDisk.cs
@@ -7,8 +7,9 @@ using System;
 
 public class TypeDataDisk : MonoBehaviour
 {
+    private DiskWithoutMono withoutMono = new DiskWithoutMono();
     [SerializeField]char diskName;
-    [SerializeField]private List<TypeDataFolder> folders = new List<TypeDataFolder>();
+    [SerializeField]private List<FolderWithoutMono> folders = new List<FolderWithoutMono>();
     public int foldersCount;
 
     private JsonSerializerSettings _settings = new JsonSerializerSettings
@@ -27,21 +28,57 @@ public class TypeDataDisk : MonoBehaviour
         SaveDataToJson();
     }
 
-    public void AddFolder(TypeDataFolder folder) 
+    public void AddFolder(FolderWithoutMono folder) 
     {
         folders.Add(folder);
         foldersCount = folders.Count;
+        UpdateDisk(false);
         SaveDataToJson();
+    }
+
+    private void UpdateDisk(bool res)
+    {
+        if (!res)
+        {
+            withoutMono.foldersCount = foldersCount;
+            withoutMono.list = folders;
+            withoutMono.nameDisk = diskName;
+        }
+        else
+        {
+            diskName = withoutMono.nameDisk;
+            folders = withoutMono.list;
+            foldersCount = withoutMono.foldersCount;
+        }
+        
     }
 
     public void SaveDataToJson()
     {
-       
+        PlayerPrefs.SetString("diskName", Convert.ToString(diskName));
+        string path = Application.persistentDataPath;
+        string fileName = string.Join("_", (diskName.ToString() + ".json").Split(Path.GetInvalidFileNameChars()));
+        path = Path.Combine(path, fileName);
+        string obj = JsonConvert.SerializeObject(withoutMono, _settings);
+        File.WriteAllText(path,obj);
+        Debug.Log("Save to" + path);
     }
 
     public void Load()
     {
-        
-
+        diskName = Convert.ToChar(PlayerPrefs.GetString("diskName","D"));
+        string path = Application.persistentDataPath;
+        string fileName = string.Join("_", (diskName.ToString() + ".json").Split(Path.GetInvalidFileNameChars()));
+        path = Path.Combine(path, fileName);
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            withoutMono = JsonConvert.DeserializeObject<DiskWithoutMono>(json);
+            if(withoutMono == null)
+            {
+                withoutMono = new DiskWithoutMono();
+            }
+            UpdateDisk(true);
+        }
     }
 }
